d8a02723f6feb0d448008042e9b1dcbc39f4ae9b
Victor Yamchinov <kodiers@gmail.com>
2019-07-12 01:56:59 +0300

    added process coins

--- app/process_data.py
+++ app/process_data.py
@@ -5,14 +5,19 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def get_data():
+def get_data(query_string):
     conn = http.client.HTTPSConnection('api.coinmarketcap.com')
-    conn.request("GET", "/v1/ticker/bitcoin/")
+    conn.request("GET", "{}".format(query_string))
     res = conn.getresponse()
     data = res.read()
     return json.loads(data.decode('utf-8'))
 
 
 def process():
-    data = get_data()
+    data = get_data('/v1/ticker/bitcoin/')
     return data[0]
+
+
+def process_coins():
+    data = get_data('/v1/ticker/?limit=10')
+    return data
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request
 
 from . import app
-from .process_data import process
+from .process_data import process, process_coins
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', data=process())
+    return render_template('index.html', data=process(), process_coins=process_coins())
