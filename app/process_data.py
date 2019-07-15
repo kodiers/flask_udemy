24bcfacad68c195afb4887a2b9a3599826eb573d
Victor Yamchinov <kodiers@gmail.com>
2019-07-16 02:27:18 +0300

    start with post

--- app/process_data.py
+++ app/process_data.py
@@ -18,6 +18,14 @@ def process():
     return data[0]
 
 
+def calculate_coin(price_usd):
+    return float(price_usd) > 40
+
+
 def process_coins():
     data = get_data('/v1/ticker/?limit=10')
-    return data
+    all_coins = []
+    for coin in data:
+        coin["isover40"] = calculate_coin(coin['price_usd'])
+        all_coins.append(coin)
+    return all_coins
--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ from . import app
 from .process_data import process, process_coins
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return request.form['cointype']
     return render_template('index.html', data=process(), process_coins=process_coins())
